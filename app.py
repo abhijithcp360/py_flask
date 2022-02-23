f31b589667d6a66d8d57be5d9b128884e8acf26f
abh <abhijith.saji@contactpoint360.com>
2022-02-23 16:23:25 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    msg = ''
+    return render_template('index.html', msg=msg)
+
+@app.route('/logout')
+def logout():
+   session.pop('loggedin', None)
+   session.pop('id', None)
+   session.pop('username', None)
+   return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    msg = ''
+    return render_template('register.html', msg=msg)
+
+@app.route('/home')
+def home():
+    if 'loggedin' in session:
+        return render_template('home.html', username=session['username'])
+    return redirect(url_for('login'))
